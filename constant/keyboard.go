e8c2bb5e6d8eb3135aac073b5854fdbe16773bb2
Gol3vka <gol3vka@163.com>
2025-05-20 23:13:07 +0800

    fix: incorrect key name for "-/_"

--- constant/keyboard.go
+++ constant/keyboard.go
@@ -13,7 +13,7 @@ var (
 
 	// KeyToPressedPos is more friendly to edit and read
 	KeyToPressedPos = [StringCnt]map[string]int{
-		{"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "0": 10, "minus (firefox), mute/unmute": 11},
+		{"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "0": 10, "dash": 11},
 		{"q": 1, "w": 2, "e": 3, "r": 4, "t": 5, "y": 6, "u": 7, "i": 8, "o": 9, "p": 10},
 		{"a": 1, "s": 2, "d": 3, "f": 4, "g": 5, "h": 6, "j": 7, "k": 8, "l": 9},
 		{"z": 1, "x": 2, "c": 3, "v": 4, "b": 5, "n": 6, "m": 7, "comma": 8},
